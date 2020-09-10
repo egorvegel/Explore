b9d7903104631d21639f64bfbecee8a9cfb58029
egorvegel <egor.vegel@mail.ru>
2020-09-10 12:25:27 +0600

    script

---
+++ js/script.js
@@ -0,0 +1,37 @@
+$(function () {
+	/* BTN */
+	let btnUp = $('.btnUp');
+	$(btnUp).on('click', function (evt) {
+		evt.preventDefault();
+
+		$('html').animate({
+			scrollTop: 0,
+		}, 300)
+	})
+
+	function checkBtn() {
+		if ($(this).scrollTop() > 900) {
+			$(btnUp).removeClass('unvisible');
+		} else {
+			$(btnUp).addClass('unvisible');
+		}
+	}
+
+	checkBtn();
+	$(window).on('scroll', function () {
+		checkBtn();
+	})
+
+	/* Меню */
+	$('.menuMobile').on('click', function (evt) {
+		evt.preventDefault();
+		if($(".nav_flex").css('display') == 'block'){
+			$('.nav_flex').hide(300);
+		}else{
+			$('.nav_flex').show(300);
+		}
+	})
+
+	/* Переходы по ссылкам */
+
+})
